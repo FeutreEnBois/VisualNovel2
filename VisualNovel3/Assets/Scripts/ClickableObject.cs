b7ed9ae6bd3269ba0a4aac16bcab822997dcf2cb
Maxime Derosier <75376645+FeutreEnBois@users.noreply.github.com>
2021-12-08 10:19:14 +0100

    clickableObject

---
+++ VisualNovel3/Assets/Scripts/ClickableObject.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObject : MonoBehaviour
+{
+    public string say = "Berret \"Heyyyyyyyyyy\"";
+
+    public string Name;
+
+    public bool preuve = false;
+}
---
+++ VisualNovel3/Assets/Scripts/GameplayManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameplayManager : MonoBehaviour
+{
+
+    public GameObject[] Clickable;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InitPointAndClickScene()
+    {
+        //Change Background To point and click background
+
+        // Clickable object setActive()
+
+    }
+
+    public void ClickOnClickableObject()
+    {
+        // Add something to say
+
+        //Add preuve to preuve
+
+        // change color on hightlighted
+
+    }
+
+}
